df7122f7e917cd317d7395944f9c39679b8ea18a
Rob Prouse <rob@prouse.org>
2011-03-08 15:06:33 -0500

    Solved problem 13

---
+++ ProjectEuler.Test/Math/BigIntTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Project.Euler.Math;
+
+namespace ProjectEuler.Test.Math
+{
+   [TestFixture]
+   public class BigIntTest
+   {
+      [Test]
+      public void TestDefaultConstructor()
+      {
+         BigInt bi = new BigInt();
+         Assert.AreEqual( "0", bi.ToString() );
+      }
+
+      [TestCase( "", "0" )]
+      [TestCase( "0", "0" )]
+      [TestCase( "1", "1" )]
+      [TestCase( "100", "100" )]
+      [TestCase( "101", "101" )]
+      [TestCase( "1234567890", "1234567890" )]
+      [TestCase( "01", "1" )]
+      [TestCase( "000010001", "10001" )]
+      public void TestConstructor( string value, string expected )
+      {
+         BigInt bi = new BigInt( value );
+         Assert.AreEqual( expected, bi.ToString() );
+      }
+
+      [TestCase( "", "", "0" )]
+      [TestCase( "0", "0", "0" )]
+      [TestCase( "0", "1", "1" )]
+      [TestCase( "9", "1", "10" )]
+      [TestCase( "99", "1", "100" )]
+      [TestCase( "1", "99", "100" )]
+      [TestCase( "99", "99", "198" )]
+      [TestCase( "100", "100", "200" )]
+      [TestCase( "987654321", "987654321", "1975308642" )]
+      [TestCase( "987654321", "987654321987654321", "987654322975308642" )]
+      [TestCase( "987654321987654321", "987654321", "987654322975308642" )]
+      public void TestAdd( string a, string b, string expected )
+      {
+         BigInt bi = new BigInt( a );
+         bi.Add( b );
+         Assert.AreEqual( expected, bi.ToString() );
+      }
+   }
+}
---
+++ ProjectEuler/Math/BigInt.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project.Euler.Math
+{
+   public class BigInt
+   {
+      private List<int> _digits = new List<int>();
+
+      public BigInt()
+      {
+         _digits.Add(0);
+      }
+
+      public BigInt( string value )
+      {
+         for ( int i = value.Length - 1; i >= 0; i-- )
+         {
+            int d = value[i] - '0';
+            if ( d >= 0 && d <= 9 )
+            {
+               _digits.Add( d );
+            }
+         }
+      }
+
+      public void Add( string value )
+      {
+         BigInt i = new BigInt( value );
+         Add(i);
+      }
+
+      public void Add( BigInt value )
+      {
+         int aCount = _digits.Count;
+         int bCount = value._digits.Count;
+         int carry = 0;
+         for ( int i = 0; i < System.Math.Max( aCount, bCount ); i++ )
+         {
+            int a = 0;
+            int b = 0;
+            if ( aCount > i )
+               a = _digits[i];
+            if ( bCount > i )
+               b = value._digits[i];
+            int sum = a + b + carry;
+            int d;
+            carry = System.Math.DivRem(sum, 10, out d);
+
+            if ( aCount > i )
+               _digits[i] = d;
+            else
+               _digits.Add(d);
+         }
+         if ( carry > 0 )
+            _digits.Add(carry);
+      }
+
+      public override string ToString()
+      {
+         StringBuilder str = new StringBuilder(_digits.Count);
+         for ( int i = _digits.Count - 1; i >= 0; i-- )
+         {
+            int d = _digits[i];
+            str.Append(d);
+         }
+         string retval = str.ToString();
+         
+         if ( retval.Length == 0 )
+            return "0";
+
+         if ( retval.Length == 1 )
+            return retval;
+
+         return retval.TrimStart('0');
+      }
+   }
+}
