4289a11b8fb52e956b7534009348ac5ce43e9862
Rob Prouse <rob@prouse.org>
2011-03-10 11:47:46 -0500

    Minor changes to problem 15

--- ProjectEuler/Problems/Problem015.cs
+++ ProjectEuler/Problems/Problem015.cs
@@ -10,12 +10,12 @@ namespace Project.Euler.Problems
     {
         public int ProblemNumber
         {
-            get { return ; }
+            get { return 15; }
         }
 
         public string Description
         {
-            get { return ""; }
+            get { return "Starting in the top left corner in a 20 by 20 grid, how many routes are there to the bottom right corner?"; }
         }
 
         public override string ToString()
@@ -23,6 +23,12 @@ namespace Project.Euler.Problems
             return String.Format("Problem {0:000}", ProblemNumber);
         }
 
+        // Pascal's triangle
+        // n --> #
+        // 2 --> 6
+        // 3 --> 20
+        // 4 --> 70
+        // 5 --> 252
         public long Solve()
         {
             return 0;
